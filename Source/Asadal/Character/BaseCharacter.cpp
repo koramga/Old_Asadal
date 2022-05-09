3dcc684724599a73440608c0e75d5f3812702bc8
koramga <koramga@gmail.com>
2022-05-09 17:59:34 +0900

    필수 GAS 관련 함수 추가 및 Ability 적용

--- Source/Asadal/Character/BaseCharacter.cpp
+++ Source/Asadal/Character/BaseCharacter.cpp
@@ -2,7 +2,7 @@
 
 
 #include "BaseCharacter.h"
-
+#include "Asadal/GAS/AttributeSet/BaseCharacterAttributeSet.h"
 
 // Sets default values
 ABaseCharacter::ABaseCharacter()
@@ -13,11 +13,124 @@ ABaseCharacter::ABaseCharacter()
 	GASComponent = CreateDefaultSubobject<UGASComponent>("GASComponent"); 
 }
 
+UAbilitySystemComponent* ABaseCharacter::GetAbilitySystemComponent() const
+{
+	return GASComponent;
+}
+
+void ABaseCharacter::InitializeAbility(TSubclassOf<UGameplayAbility> AbilityToGet, int32 AbilityLevel)
+{
+	if(IsValid(GASComponent))
+	{
+		if(HasAuthority() && IsValid(AbilityToGet))
+		{
+			GASComponent->GiveAbility(FGameplayAbilitySpec(AbilityToGet, AbilityLevel, 0));
+		}
+
+		GASComponent->InitAbilityActorInfo(this, this);
+	}
+}
+
+void ABaseCharacter::GetHealthValues(float& Health, float& MaxHealth)
+{
+	if(IsValid(BaseCharacterAttributeSet))
+	{
+		Health = BaseCharacterAttributeSet->GetHealth();
+		MaxHealth = BaseCharacterAttributeSet->GetMaxHealth();		
+	}
+}
+
+void ABaseCharacter::SetHealthValues(float NewHealth, float NewMaxHealth)
+{
+	if(IsValid(GASComponent))
+	{
+		GASComponent->ApplyModToAttribute(BaseCharacterAttributeSet->GetHealthAttribute(), EGameplayModOp::Override, NewHealth);
+		GASComponent->ApplyModToAttribute(BaseCharacterAttributeSet->GetMaxHealthAttribute(), EGameplayModOp::Override, NewMaxHealth);		
+	}
+}
+
+void ABaseCharacter::GetManaValues(float& Mana, float& MaxMana)
+{
+	if(IsValid(BaseCharacterAttributeSet))
+	{
+		Mana = BaseCharacterAttributeSet->GetMana();
+		MaxMana = BaseCharacterAttributeSet->GetMaxMana();		
+	}
+}
+
+void ABaseCharacter::SetManaValues(float NewMana, float NewMaxMana)
+{
+	if(IsValid(GASComponent))
+	{
+		GASComponent->ApplyModToAttribute(BaseCharacterAttributeSet->GetManaAttribute(), EGameplayModOp::Override, NewMana);
+		GASComponent->ApplyModToAttribute(BaseCharacterAttributeSet->GetMaxManaAttribute(), EGameplayModOp::Override, NewMaxMana);		
+	}
+}
+
+void ABaseCharacter::ChangeAbilityLevelWithTags(FGameplayTagContainer TagContainer, int32 NewAbilityLevel)
+{
+	if(IsValid(GASComponent))
+	{
+		TArray<FGameplayAbilitySpec*> MatchingAbilities;
+
+		GASComponent->GetActivatableGameplayAbilitySpecsByAllMatchingTags(TagContainer, MatchingAbilities, true);
+
+		for(FGameplayAbilitySpec* AbilitySpec : MatchingAbilities)
+		{
+			AbilitySpec->Level = NewAbilityLevel;
+		}
+	}
+}
+
+void ABaseCharacter::CancelAbilityWithTags(FGameplayTagContainer WithTags, FGameplayTagContainer WithoutTags)
+{
+	if(IsValid(GASComponent))
+	{
+		GASComponent->CancelAbilities(&WithTags, &WithoutTags, nullptr);
+	}
+}
+
+void ABaseCharacter::AddLooseGameplayTag(FGameplayTag TagToAdd)
+{
+	if(IsValid(GASComponent))
+	{
+		GASComponent->AddLooseGameplayTag(TagToAdd);
+		GASComponent->SetTagMapCount(TagToAdd, 1);
+	}
+}
+
+void ABaseCharacter::RemoveLooseGameplayTag(FGameplayTag TagToRemove)
+{
+	if(IsValid(GASComponent))
+	{
+		GASComponent->RemoveLooseGameplayTag(TagToRemove);
+	}
+}
+
+void ABaseCharacter::ApplyGEToTargetData(const FGameplayEffectSpecHandle& GESpec,
+	const FGameplayAbilityTargetDataHandle& TargetDataHandle)
+{
+	for(TSharedPtr<FGameplayAbilityTargetData> Data : TargetDataHandle.Data)
+	{
+		Data->ApplyGameplayEffectSpec(*GESpec.Data.Get());
+	}
+}
+
 // Called when the game starts or when spawned
 void ABaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if(IsValid(GASComponent))
+	{
+		BaseCharacterAttributeSet = GASComponent->GetSet<UBaseCharacterAttributeSet>();
+
+		if(IsValid(BaseCharacterAttributeSet))
+		{
+			GASComponent->GetGameplayAttributeValueChangeDelegate(BaseCharacterAttributeSet->GetHealthAttribute()).AddUObject(this, &ABaseCharacter::__OnHealthChangedNative);
+			GASComponent->GetGameplayAttributeValueChangeDelegate(BaseCharacterAttributeSet->GetManaAttribute()).AddUObject(this, &ABaseCharacter::__OnManaChangedNative);			
+		}		
+	}
 }
 
 // Called every frame
@@ -26,7 +139,20 @@ void ABaseCharacter::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
-UAbilitySystemComponent* ABaseCharacter::GetAbilitySystemComponent() const
+void ABaseCharacter::OnHealthChanged(const FOnAttributeChangeData& Data)
 {
-	return GASComponent;
+}
+
+void ABaseCharacter::OnManaChanged(const FOnAttributeChangeData& Data)
+{
+}
+
+void ABaseCharacter::__OnHealthChangedNative(const FOnAttributeChangeData& Data)
+{
+	OnHealthChanged(Data);
+}
+
+void ABaseCharacter::__OnManaChangedNative(const FOnAttributeChangeData& Data)
+{
+	OnManaChanged(Data);
 }
--- Source/Asadal/Character/BaseCharacter.h
+++ Source/Asadal/Character/BaseCharacter.h
@@ -17,19 +17,38 @@ class ASADAL_API ABaseCharacter : public ACharacter, public IAbilitySystemInterf
 public:
 	// Sets default values for this character's properties
 	ABaseCharacter();
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	void InitializeAbility(TSubclassOf<UGameplayAbility> AbilityToGet, int32 AbilityLevel);
+	void GetHealthValues(float& Health, float& MaxHealth);
+	void SetHealthValues(float NewHealth, float NewMaxHealth);
+	void GetManaValues(float& Mana, float& MaxMana);
+	void SetManaValues(float NewMana, float NewMaxMana);
+	void ChangeAbilityLevelWithTags(FGameplayTagContainer TagContainer, int32 NewAbilityLevel);
+	void CancelAbilityWithTags(FGameplayTagContainer WithTags, FGameplayTagContainer WithoutTags);
+	void AddLooseGameplayTag(FGameplayTag TagToAdd);
+	void RemoveLooseGameplayTag(FGameplayTag TagToRemove);
+	void ApplyGEToTargetData(const FGameplayEffectSpecHandle& GESpec, const FGameplayAbilityTargetDataHandle& TargetDataHandle);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:
+	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-public :
-	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+protected:
+	void OnHealthChanged(const FOnAttributeChangeData& Data);
+	void OnManaChanged(const FOnAttributeChangeData& Data);
 
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "BaseCharacter")
 	UGASComponent* GASComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="BaseCharacter")
+	const class UBaseCharacterAttributeSet*	BaseCharacterAttributeSet;
+
+private :
+	void __OnHealthChangedNative(const FOnAttributeChangeData& Data);
+	void __OnManaChangedNative(const FOnAttributeChangeData& Data);
 };
