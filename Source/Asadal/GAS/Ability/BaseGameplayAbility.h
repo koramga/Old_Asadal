cb8427ed0e11fecc42a30760cec7aa2f2bffefdd
koramga <koramga@gmail.com>
2022-05-10 18:56:26 +0900

    GA_Melee BP 생성

--- Source/Asadal/GAS/Ability/BaseGameplayAbility.h
+++ Source/Asadal/GAS/Ability/BaseGameplayAbility.h
@@ -19,4 +19,9 @@ protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	virtual void EndAbility(bool bWasCancelled = false, bool bReplicateEndAbility = true);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Setup")
+	UTexture*	IconTexture;
+	
 };
