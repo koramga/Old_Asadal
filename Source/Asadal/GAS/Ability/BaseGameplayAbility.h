6b02bbbc9ff1c1952cdbaa78441b22db67130db7
koramga <koramga@gmail.com>
2022-05-10 12:43:24 +0900

    BaseGameplayAbility Create

---
+++ Source/Asadal/GAS/Ability/BaseGameplayAbility.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "UObject/Object.h"
+#include "BaseGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASADAL_API UBaseGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	virtual void EndAbility(bool bWasCancelled = false, bool bReplicateEndAbility = true);
+};
